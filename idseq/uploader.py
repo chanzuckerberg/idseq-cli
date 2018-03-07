75a75de285d419ee1fc05ea765b57c81230b8d80
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-03-07 14:05:07 -0800

    try

--- idseq/uploader.py
+++ idseq/uploader.py
@@ -121,7 +121,7 @@ def upload(
     else:
         print('failed %s' % resp.status_code)
         print(resp.json())
-        raise
+        raise Exception('')
 
     if source_type == 'local':
         data = resp.json()
