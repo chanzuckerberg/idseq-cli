adeffa60d59f624e33f3b27102ff9e5e80f4c356
Julie Han <jhan@chanzuckerberg.com>
2020-08-27 10:14:15 -0700

    Bump cli version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='idseq',
-      version='0.8.8',
+      version='0.8.10',
       description='IDseq CLI',
       url='http://github.com/chanzuckerberg/idsdeq-cli',
       author='Chan Zuckerberg Initiative, LLC',
