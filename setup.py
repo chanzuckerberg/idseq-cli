a0643d17afb6c70be040737e29c099009de2adde
David Rissato Cruz <davidrc@gmail.com>
2020-12-08 13:15:11 -0800

    Fix file split issue for uploads larger than 5GB (#75)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='idseq',
-      version='0.8.11',
+      version='0.8.12',
       description='IDseq CLI',
       url='http://github.com/chanzuckerberg/idsdeq-cli',
       author='Chan Zuckerberg Initiative, LLC',
