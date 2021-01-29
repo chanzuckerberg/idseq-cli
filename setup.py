e2aa9241dd2de30ce4ae6eef9dafc29085462fd1
Jonathan Sheu <jsheu@chanzuckerberg.com>
2021-01-28 16:14:49 -0800

    Use faster version of GET projects.json

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='idseq',
-      version='0.8.12',
+      version='0.8.13',
       description='IDseq CLI',
       url='http://github.com/chanzuckerberg/idsdeq-cli',
       author='Chan Zuckerberg Initiative, LLC',
